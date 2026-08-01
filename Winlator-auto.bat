fb005262ce2fbe1a88c0b47183b5997562f49b55
Coolcricri <139659427+Coolcricri@users.noreply.github.com>
2026-08-01 14:48:25 +0300

    Create Winlator-auto.bat
    Batch file specifically to use on Winlator with muipatch.exe

---
+++ Winlator-auto.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set "GAMES_DIR=C:\Program Files\Microsoft Games"
+
+"%~dp0Windows 7 Games for Windows 11, 10 and 8-4.2-setup.exe" /SP- /SILENT
+"%~dp0muipatch.exe" "%GAMES_DIR%"
+
+for /r "%GAMES_DIR%" %%F in (*.png *.res *_original.exe *_original.dll) do del /f /q "%%F"
+
+xcopy /E /I /Y "%GAMES_DIR%" "D:\Microsoft Games\"
+
+endlocal
